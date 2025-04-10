49b019937b1bbba90d33483a95406a260d57ba26
Ditar <DitarRogozhnikov@gmail.com>
2025-04-10 18:21:47 +0300

    InitialCommit

---
+++ build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.4.4"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.micro"
+version = "0.0.1"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	//Security
+	implementation("org.springframework.boot:spring-boot-starter-security:3.4.4")
+	implementation("org.springframework.boot:spring-boot-starter-web:3.4.4")
+	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+
+	//Eureka
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:4.2.1")
+
+	//Postgres
+	implementation("org.postgresql:postgresql:42.7.5")
+	runtimeOnly("org.flywaydb:flyway-database-postgresql:11.7.0")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.4.4")
+
+	//Addiction
+	implementation("io.micrometer:micrometer-registry-prometheus:1.14.5")
+	implementation("org.springframework.boot:spring-boot-starter-actuator:3.4.4")
+	implementation("org.springframework.boot:spring-boot-devtools:3.4.4")
+	implementation("org.springframework.boot:spring-boot-configuration-processor:3.4.4")
+
+	compileOnly("org.projectlombok:lombok:1.18.38")
+
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/kotlin/com/micro/security/appconfig/AppInfo.kt
@@ -0,0 +1,69 @@
+package com.micro.security.appconfig
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.event.ContextClosedEvent
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class AppInfo {
+    @Value("\${spring.application.name}")
+    private val appName: String? = null
+
+    @Value("\${spring.profiles.active}")
+    private val activeProfile: String? = null
+
+    @Value("\${server.port}")
+    private val serverPort: String? = null
+
+    @Value("\${server.ip}")
+    private val serverIp: String? = null
+
+    private val startDelimiter = "=============^_^============="
+    private val stopDelimiter = "============================="
+
+    private val ln = "\n"
+    private val startTime = Date()
+
+    @EventListener(ApplicationReadyEvent::class)
+    fun printAppData() {
+        val builder = startDelimiter + ln +
+                "Application: " + appName + ln +
+                "Configuration: " + activeProfile + ln +
+                "Server port: " + serverPort + ln +
+                "Server ip: " + serverIp + ln +
+                "Start date time: " + Date() + ln +
+                stopDelimiter
+        println(builder)
+    }
+
+    @EventListener(ContextClosedEvent::class)
+    fun onContextClosedEvent(contextClosedEvent: ContextClosedEvent) {
+        val builder = StringBuilder()
+        builder.append(startDelimiter).append(ln)
+        builder.append("Start date time: ").append(Date(contextClosedEvent.timestamp)).append(ln)
+        builder.append(calculateUpTime(contextClosedEvent.timestamp)).append(ln)
+        builder.append(stopDelimiter)
+        println(builder.toString())
+    }
+
+    private fun calculateUpTime(endMilisec: Long): StringBuilder {
+        var fullUpTime = (endMilisec - startTime.time) / 1000
+        val days = fullUpTime / 60 / 60 / 24
+        fullUpTime = fullUpTime - (days * 24 * 60 * 60)
+        val hours = fullUpTime / 60 / 60
+        fullUpTime = fullUpTime - (hours * 60 * 60)
+        val minutes = fullUpTime / 60
+        fullUpTime = fullUpTime - (hours * 60)
+        val seconds = fullUpTime
+        val builder = StringBuilder()
+        builder.append("Full up time : ")
+        builder.append(days).append(" days, ")
+        builder.append(hours).append(" hours, ")
+        builder.append(minutes).append(" minutes, ")
+        builder.append(seconds).append(" seconds, ")
+        return builder
+    }
+}
